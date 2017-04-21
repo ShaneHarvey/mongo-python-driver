738bf0c6939095d37dab5ba1943a9f8c5b6013c1
Shane Harvey <shane.harvey@mongodb.com>
2017-04-20 17:21:11 -0700

    Only run coverage if it's installed.

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -53,17 +53,17 @@ $PYTHON -c 'import sys; print(sys.version)'
 # Run the tests, and store the results in Evergreen compatible XUnit XML
 # files in the xunit-results/ directory.
 
-COVERAGE="$(command -v coverage)"
-if [ -z "$COVERAGE" ]; then
-    echo "INFO: coverage is not installed, running tests without coverage..."
-else
+COVERAGE="-m coverage run"
+if $PYTHON $COVERAGE --help; then
     echo "INFO: coverage is installed, running tests with coverage..."
-    COVERAGE="$COVERAGE run"
+else
+    echo "INFO: coverage is not installed, running tests without coverage..."
+    COVERAGE=""
 fi
 
 $PYTHON setup.py clean
 if [ -z "$GREEN_FRAMEWORK" ]; then
-    $PYTHON $COVERAGE run setup.py $C_EXTENSIONS test $OUTPUT
+    $PYTHON $COVERAGE setup.py $C_EXTENSIONS test $OUTPUT
 else
     # --no_ext has to come before "test" so there is no way to toggle extensions here.
     $PYTHON green_framework_test.py $GREEN_FRAMEWORK $OUTPUT
