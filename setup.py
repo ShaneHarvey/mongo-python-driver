a7a274939d6be3ebf739da58813b4c7d3b7c4950
Pascal Corpet <pascal+github@corpet.net>
2019-03-06 15:39:54 +0100

    Add stub files for bson and fix the ones for pymongo.

--- setup.py
+++ setup.py
@@ -428,5 +428,9 @@ setup(
               "doc": doc,
               "test": test},
     extras_require=extras_require,
+    package_data={
+        'bson': ['bson/py.typed', 'bson/*.pyi'],
+        'pymongo': ['bson/py.typed', 'bson/*.pyi'],
+    },
     **extra_opts
 )
