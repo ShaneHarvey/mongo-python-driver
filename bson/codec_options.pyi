d241d499643e6199cc58b614661c42dcb50efa13
Shane Harvey <shnhrv@gmail.com>
2020-10-20 13:26:43 -0700

    PYTHON-1439 Update stub files from typeshed POC
    https://github.com/ShaneHarvey/typeshed/commit/d7abcf91db9cc8ecaabfc4f0a3fe4ed934a4ebd8

--- bson/codec_options.pyi
+++ bson/codec_options.pyi
@@ -1,34 +1,46 @@
+import abc
 import datetime
+from collections import namedtuple
 from typing import Any, Callable, Iterable, NamedTuple, Optional, Union
 
-class TypeEncoder:
+class TypeEncoder(abc.ABC, metaclass=abc.ABCMeta):
     @property
+    @abc.abstractmethod
     def python_type(self) -> Any: ...
+    @abc.abstractmethod
     def transform_python(self, value: Any) -> Any: ...
 
-class TypeDecoder:
+class TypeDecoder(abc.ABC, metaclass=abc.ABCMeta):
     @property
+    @abc.abstractmethod
     def bson_type(self) -> Any: ...
+    @abc.abstractmethod
     def transform_bson(self, value: Any) -> Any: ...
 
-class TypeCodec(TypeEncoder, TypeDecoder): ...
+class TypeCodec(TypeEncoder, TypeDecoder, metaclass=abc.ABCMeta): ...
+
 Codec = Union[TypeEncoder, TypeDecoder, TypeCodec]
 Fallback = Callable[[Any], Any]
 
 class TypeRegistry:
-    def __init__(self, type_codecs: Optional[Iterable[Codec]]=..., fallback_encoder: Optional[Fallback]=...) -> None: ...
+    def __init__(self, type_codecs: Optional[Iterable[Codec]] = ..., fallback_encoder: Optional[Fallback] = ...) -> None: ...
+    def __eq__(self, other: Any) -> Any: ...
 
-_options_base = NamedTuple('CodecOptions', [
-    ('document_class', type),
-    ('tz_aware', bool),
-    ('uuid_representation', int),
-    ('unicode_decode_error_handler', str),
-    ('tzinfo', Optional[datetime.tzinfo]),
-    ('type_registry', TypeRegistry)
-])
+_options_base = namedtuple(
+    "_options_base",
+    ["document_class", "tz_aware", "uuid_representation", "unicode_decode_error_handler", "tzinfo", "type_registry"],
+)
 
 class CodecOptions(_options_base):
-    def __new__(cls: Any, document_class: type=..., tz_aware: bool=..., uuid_representation: Optional[int]=..., unicode_decode_error_handler: str=..., tzinfo: Optional[datetime.tzinfo]=..., type_registry: Optional[TypeRegistry]=...) -> CodecOptions: ...
+    def __new__(
+        cls: Any,
+        document_class: Optional[type] = ...,
+        tz_aware: Optional[bool] = ...,
+        uuid_representation: Optional[int] = ...,
+        unicode_decode_error_handler: Optional[str] = ...,
+        tzinfo: Optional[datetime.tzinfo] = ...,
+        type_registry: Optional[TypeRegistry] = ...,
+    ) -> CodecOptions: ...
     def with_options(self, **kwargs: Any) -> CodecOptions: ...
 
-DEFAULT_CODEC_OPTIONS: CodecOptions
+DEFAULT_CODEC_OPTIONS: Any
