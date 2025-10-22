88ccee4f1a0a413cb139abe86a396b5c3efbc84f
Shane Harvey <shnhrv@gmail.com>
2025-10-22 11:48:49 -0700

    backpressure-1ms-1.25g-500ms

--- pymongo/asynchronous/client_session.py
+++ pymongo/asynchronous/client_session.py
@@ -474,7 +474,7 @@ _UNKNOWN_COMMIT_ERROR_CODES: frozenset = _RETRYABLE_ERROR_CODES | frozenset(  # 
 # default value of MongoDB's `transactionLifetimeLimitSeconds` parameter.
 _WITH_TRANSACTION_RETRY_TIME_LIMIT = 120
 _BACKOFF_MAX = 0.5
-_BACKOFF_INITIAL = 0.005  # 5ms initial backoff
+_BACKOFF_INITIAL = 0.001  # 1ms initial backoff
 
 
 def _within_time_limit(start_time: float) -> bool:
@@ -708,7 +708,7 @@ class AsyncClientSession:
         while True:
             if retry:  # Implement exponential backoff on retry.
                 jitter = random.random()  # noqa: S311
-                backoff = jitter * min(_BACKOFF_INITIAL * (1.5**retry), _BACKOFF_MAX)
+                backoff = jitter * min(_BACKOFF_INITIAL * (1.25**retry), _BACKOFF_MAX)
                 await asyncio.sleep(backoff)
             retry += 1
             await self.start_transaction(
